3e3d0ba535e8f0aaab655297797fae99b7acd32f
fabianpy <naibaf.martinez@gmail.com>
2018-03-20 13:36:15 -0400

    se agregó admin de IVA

--- apps/principal/admin.py
+++ apps/principal/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
-from apps.principal.models import Moneda
+from apps.principal.models import Moneda, IVA
 
 
 @admin.register(Moneda)
 class MonedaAdmin(admin.ModelAdmin):
     list_display = ['codigo', 'nombre', 'decimales', 'abreviatura', 'simbolo', 'habilitado']
+    list_per_page = 15
+
+
+@admin.register(IVA)
+class IVAAdmin(admin.ModelAdmin):
+    list_display = ['porcentaje', 'habilitado']
     list_per_page = 15
